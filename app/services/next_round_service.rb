b93998ebb178777f1680a722dd4f48f6d5b9133e
Sebastian Wakefield <seb7wake@gmail.com>
2024-06-07 11:56:25 -0700

    fixing next round modelling logic

--- app/services/next_round_service.rb
+++ app/services/next_round_service.rb
@@ -5,24 +5,25 @@ class NextRoundService
     end
 
     def get_instrument_buying_power()
-        buying_power = 0
+        shares = 0
         outstanding_shares = @company.fully_diluted_total
         # may need to adust discounted share price calculation
         share_price = (@company.next_round.pre_money_valuation.to_f / outstanding_shares)
         pre_money_instruments = @company.financial_instruments.where(instrument_type: ["Pre-Money SAFE", "Pre-Money Convertible Note", "Convertible Note"]).where('valuation_cap > ?', 0).where('principal > ?', 0)
         pre_money_instruments.each do |instrument|
-          price = instrument.discount_rate > 0 && @company.next_round.pre_money_valuation <= instrument.valuation_cap ? share_price * (1- (instrument.discount_rate.to_f / 100)) : share_price 
-          buying_power += (instrument.principal / instrument.valuation_cap.to_f) * outstanding_shares * price
+          discounted_share_price = share_price
+          discounted_share_price -= share_price * (instrument.discount_rate.to_f / 100) if instrument.discount_rate > 0 && @company.next_round.pre_money_valuation <= instrument.valuation_cap
+          valuation_cap_share_price = instrument.valuation_cap.to_f / @company.fully_diluted_total
           outstanding_shares += (instrument.principal / instrument.valuation_cap.to_f) * outstanding_shares
+          shares += instrument.principal / [valuation_cap_share_price, discounted_share_price].min
         end
         post_money_instruments = @company.financial_instruments.where(instrument_type: ["Post-Money SAFE"]).where('valuation_cap > ?', 0).where('principal > ?', 0)
+        total_percent_owed_post_money_safes = post_money_instruments.sum { |instrument| instrument.principal / instrument.valuation_cap.to_f }
+        outstanding_shares_new = outstanding_shares / (1.0 - total_percent_owed_post_money_safes).to_f
         post_money_instruments.each do |instrument|
-          new_fully_diluted_shares = outstanding_shares + (((instrument.principal / instrument.valuation_cap.to_f) * outstanding_shares) / share_price)
-          new_share_price = ((instrument.principal / instrument.valuation_cap.to_f) * outstanding_shares) / new_fully_diluted_shares.to_f
-          new_share_price = instrument.discount_rate > 0 && @company.next_round.pre_money_valuation <= instrument.valuation_cap ? new_share_price * (1 - (instrument.discount_rate.to_f / 100)) : new_share_price 
-          buying_power += new_share_price * outstanding_shares
+          shares += (instrument.principal / instrument.valuation_cap.to_f) * outstanding_shares_new
         end
-        buying_power
+        shares * share_price
       end
 
     def construct_next_round()
