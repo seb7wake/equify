05b807ec3f8fd70fdc23f30ee79452960e04e60b
Sebastian Wakefield <seb7wake@gmail.com>
2024-06-16 19:25:45 -0700

    updating webpack config for better load times

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,17 +1,44 @@
-// See the shakacode/shakapacker README and docs directory for advice on customizing your webpackConfig.
 const { generateWebpackConfig } = require("shakapacker");
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 const webpackConfig = generateWebpackConfig();
 
+// Optimize CSS loading and extraction
 webpackConfig.module.rules.push({
   test: /\.scss$/,
   use: [
-    "style-loader", // Injects styles into DOM
+    MiniCssExtractPlugin.loader, // Extracts CSS into separate files
     "css-loader", // Translates CSS into CommonJS
     "sass-loader", // Compiles Sass to CSS
   ],
 });
+
+// Enable code splitting
+webpackConfig.optimization = {
+  splitChunks: {
+    chunks: "all",
+  },
+  minimize: true,
+  minimizer: [new TerserPlugin()],
+};
+
+// Enable long-term caching
+webpackConfig.output = {
+  filename: "[name].[contenthash].js",
+  path: path.resolve(__dirname, "../../public/packs"),
+  publicPath: "/packs/",
+};
+
+// Add plugins
+webpackConfig.plugins.push(
+  new MiniCssExtractPlugin({
+    filename: "[name].[contenthash].css",
+  }),
+  new CleanWebpackPlugin() // Cleans output.path directory before each build
+);
 
 webpackConfig.mode = "production";
 webpackConfig.devtool = "source-map";
@@ -20,12 +47,7 @@ webpackConfig.resolve.extensions.push(".sass");
 webpackConfig.resolve.extensions.push(".ts");
 webpackConfig.resolve.extensions.push(".tsx");
 
-// webpackConfig.output = {
-//   filename: "[name].js",
-//   sourceMapFilename: "[name].js.map",
-//   path: path.resolve(__dirname, "..", "..", "app/assets/builds"),
-// };
-
+// Entry point configuration
 webpackConfig.entry = {
   application: path.resolve(
     __dirname,
